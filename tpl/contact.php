e89cb13b01474367896172942e184fc22a54a111
vijithv-cuelogic <vijith.vijayan@cuelogic.co.in>
2016-04-11 19:45:36 +0530

    Completed contact us page map and form submition part

--- tpl/contact.php
+++ tpl/contact.php
@@ -1,129 +1,103 @@
 <?php
 error_reporting(0);
-//print_r($_POST); exit;
-if (isset($_POST['btnSubmit']))
+
+if (isset($_POST['submit']))
 {
-//print_r($_POST);
-$strFirestName = stripslashes(trim($_POST["txtName"]));
-$strEmail    = stripslashes(trim($_POST["txtEmail"]));
-$intPhoneNo    = stripslashes(trim($_POST["txtPhone"]));
-$strCompany    = stripslashes(trim($_POST["txtCompany"]));
-$strWebsite    = stripslashes(trim($_POST["txtWebsite"]));
-$strMessage    = stripslashes(trim($_POST["txtMessage"]));
-$strinterest   = stripslashes(trim($_POST["cmbIntrest"]));
-if($strFirestName == "")
-{ $strErrorMessage .= "Please enter Name.<br>";}
-if($strEmail == "")
-{$strErrorMessage .= "Please enter Email.<br>";}
-else{
-if(preg_match("/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})+$/",$strEmail) == 0)
-{
-$strErrorMessage .= "Enter valid Email Id.<br>";
-}
-}
-if($intPhoneNo == "")
-{$strErrorMessage .= "Please enter phone number.<br>";}
-if($_POST["recaptcha_response_field"])
-{
-$resp = recaptcha_check_answer ($privatekey,
-$_SERVER["REMOTE_ADDR"],
-$_POST["recaptcha_challenge_field"],
-$_POST["recaptcha_response_field"]);
-if ($resp->is_valid) {
-// nothing to do for correct captcha
-} else {
-# set the error code so that we can display it
-$strErrorMessage .= "Incorrect captcha, please re-enter.<br>";
-}
-}
-else
-{
-$strErrorMessage .= "Please Enter Captcha.<br>" ;
-}
-//print $strErrorMessage; exit; 
-if($strErrorMessage == "")
-{
-$css_lefttd = ' style="color:#0072bb; font-size:13px; font-family:Arial, Helvetica, sans-serif"';
-$css_righttd = ' style="color:#3b3b3b; font-size:13px; font-family:Arial, Helvetica, sans-serif"';
-$mailcontent ='<table width="450" border="0" cellpadding="8" cellspacing="0" style="border:1px solid #cccccc">
-<tr>
-<td colspan="2" bgcolor="#ffffff" align="right" style="color:#0072bb; font-size:13px; font-family:Arial, Helvetica, sans-serif">
-<img src="http://www.fourtech.com/images/fourtech_logo.png" width="70px" />
-</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Name :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##NAME##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Email :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##EMAIL##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Phone Number :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##PHONE##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Company Name :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##COMPANY##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Website :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##WEBSITE##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Interest :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##INTREST##</td>
-</tr>
-<tr>
-<td width="124" align="right" valign="middle" '.$css_lefttd.'>Message :</td>
-<td width="348" align="left" valign="middle" '.$css_righttd.'>##MESSAGE##</td>
-</tr>
-<tr><td width="124" align="right" valign="middle" '.$css_lefttd.'></td><td width="348" align="left" valign="middle" '.$css_righttd.'></td></tr>
-<tr><td width="124" align="right" valign="middle" '.$css_lefttd.'></td><td width="348" align="left" valign="middle" '.$css_righttd.'></td></tr>
-</table>';
-$mailcontent = str_replace("##NAME##", stripcslashes($_POST["txtName"]), $mailcontent); 
-$mailcontent = str_replace("##EMAIL##", stripcslashes($_POST["txtEmail"]), $mailcontent); 
-$mailcontent = str_replace("##PHONE##", stripcslashes($_POST["txtPhone"]), $mailcontent); 
-$mailcontent = str_replace("##COMPANY##", stripcslashes($_POST["txtCompany"]), $mailcontent);      
-$mailcontent = str_replace("##WEBSITE##", stripcslashes($_POST["txtWebsite"]), $mailcontent); 
-$mailcontent = str_replace("##INTREST##", stripcslashes($_POST["cmbIntrest"]), $mailcontent); 
-/*$mailcontent = str_replace("##BUDGET##", stripcslashes($_POST["cmbBudget"]), $mailcontent); */
-$mailcontent = str_replace("##MESSAGE##", stripcslashes($_POST["txtMessage"]), $mailcontent); 
-$Subject = $_POST["txtName"] ." contacted on fourtech.com today";
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-// Additional headers
-$headers .= 'From: Info <info@fourtech.co.in>' . "\r\n";
-$to = "champprasannagmail.com";
-mail($to, $Subject, $mailcontent, $headers);
-$mailcontent = "<HTML>
-<HEAD>
-<TITLE> New Document </TITLE>
-</HEAD>
-<BODY $css_righttd>
-<p>Hello ##NAME##,</p>
-<p>Thanks very much for contacting us at Four-Tech. We will get back to you within one business day.</p>
-<p>Meanwhile, we would appreciate,if you could send us any additional details about your requirements on champprasanna@gmail.com, so that our next interaction can be more contextual.</p>
-<p>Best Luck,<br>
-Team Four-Tech<br>
-Four-Tech India Pvt.Ltd.<br>
-http://www.fourtech.com/<br>
-India:25669874<br>
-<p>
-</BODY>
-</HTML>";
-$mailcontent = str_replace("##NAME##", stripcslashes($_POST["txtName"]), $mailcontent); 
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: Prasanna <champprasanna@gmail.com>' . "\r\n";
-$to = $_POST["txtEmail"];
-$Subject = "Thank you for contacting Four-Tech ";
-mail($to, $Subject, $mailcontent, $headers);
-$_SESSION['message'] = "mess";
-}
+  //print_r($_POST);exit;
+  $strFirestName = stripslashes(trim($_POST["txtName"]));
+  $strEmail    = stripslashes(trim($_POST["txtEmail"]));
+  $intPhoneNo    = stripslashes(trim($_POST["txtPhone"]));
+  $strCompany    = stripslashes(trim($_POST["txtCompany"]));
+  $strMessage    = stripslashes(trim($_POST["txtMessage"]));
+  if($strFirestName == ""){ 
+    $strErrorMessage .= "Please enter Name.<br>";
+  }
+
+  if($strEmail == ""){
+    $strErrorMessage .= "Please enter Email.<br>";
+  }
+  else{
+    if(preg_match("/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})+$/",$strEmail) == 0){
+      $strErrorMessage .= "Enter valid Email Id.<br>";
+    }
+  }
+  /*if($intPhoneNo == "") {
+    $strErrorMessage .= "Please enter company phone number.<br>";
+  }*/
+
+  //print $strErrorMessage; exit; 
+  if($strErrorMessage == "")
+  {
+    $css_lefttd = ' style="color:#0072bb; font-size:13px; font-family:Arial, Helvetica, sans-serif"';
+    $css_righttd = ' style="color:#3b3b3b; font-size:13px; font-family:Arial, Helvetica, sans-serif"';
+    $mailcontent ='<table width="450" border="0" cellpadding="8" cellspacing="0" style="border:1px solid #cccccc">
+    <tr>
+    <td colspan="2" bgcolor="#ffffff" align="right" style="color:#0072bb; font-size:13px; font-family:Arial, Helvetica, sans-serif">
+    <img src="http://www.fourtech.com/images/fourtech_logo.png" width="70px" />
+    </td>
+    </tr>
+    <tr>
+    <td width="124" align="right" valign="middle" '.$css_lefttd.'>Name :</td>
+    <td width="348" align="left" valign="middle" '.$css_righttd.'>##NAME##</td>
+    </tr>
+    <tr>
+    <td width="124" align="right" valign="middle" '.$css_lefttd.'>Email :</td>
+    <td width="348" align="left" valign="middle" '.$css_righttd.'>##EMAIL##</td>
+    </tr>
+    <tr>
+    <td width="124" align="right" valign="middle" '.$css_lefttd.'>Company Address :</td>
+    <td width="348" align="left" valign="middle" '.$css_righttd.'>##COMPANY##</td>
+    </tr>
+    <tr>
+    <td width="124" align="right" valign="middle" '.$css_lefttd.'>Company Phone Number :</td>
+    <td width="348" align="left" valign="middle" '.$css_righttd.'>##PHONE##</td>
+    </tr>
+    <tr>
+    <td width="124" align="right" valign="middle" '.$css_lefttd.'>Message :</td>
+    <td width="348" align="left" valign="middle" '.$css_righttd.'>##MESSAGE##</td>
+    </tr>
+    <tr><td width="124" align="right" valign="middle" '.$css_lefttd.'></td><td width="348" align="left" valign="middle" '.$css_righttd.'></td></tr>
+    <tr><td width="124" align="right" valign="middle" '.$css_lefttd.'></td><td width="348" align="left" valign="middle" '.$css_righttd.'></td></tr>
+    </table>';
+    $mailcontent = str_replace("##NAME##", stripcslashes($_POST["txtName"]), $mailcontent); 
+    $mailcontent = str_replace("##EMAIL##", stripcslashes($_POST["txtEmail"]), $mailcontent); 
+    $mailcontent = str_replace("##PHONE##", stripcslashes($_POST["txtPhone"]), $mailcontent); 
+    $mailcontent = str_replace("##COMPANY##", stripcslashes($_POST["txtCompany"]), $mailcontent);          
+    $mailcontent = str_replace("##MESSAGE##", stripcslashes($_POST["txtMessage"]), $mailcontent); 
+    $Subject = $_POST["txtName"] ." contacted on fourtech.com today";
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    // Additional headers
+    $headers .= 'From: Info <info@fourtechengineering.in>' . "\r\n";
+    $to = "champprasannagmail.com";
+    mail($to, $Subject, $mailcontent, $headers);
+    $mailcontent = "<HTML>
+    <HEAD>
+    <TITLE> New Document </TITLE>
+    </HEAD>
+    <BODY $css_righttd>
+    <p>Hello ##NAME##,</p>
+    <p>Thanks very much for contacting us at Four-Tech. We will get back to you within one business day.</p>
+    <p>Meanwhile, we would appreciate,if you could send us any additional details about your requirements on champprasanna@gmail.com, so that our next interaction can be more contextual.</p>
+    <p>Best Luck,<br>
+    Team Four-Tech<br>
+    Four-Tech India Pvt.Ltd.<br>
+    http://www.fourtech.com/<br>
+    India:25669874<br>
+    <p>
+    </BODY>
+    </HTML>";
+    $mailcontent = str_replace("##NAME##", stripcslashes($_POST["txtName"]), $mailcontent); 
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From: Prasanna <champprasanna@gmail.com>' . "\r\n";
+    $to = $_POST["txtEmail"];
+    $Subject = "Thank you for contacting Four-Tech ";
+    mail($to, $Subject, $mailcontent, $headers);
+    $_SESSION['message'] = "mess";
+  }
 }
 ?>
 <div id="maincontent"> 
@@ -168,24 +142,19 @@ unset($_POST);
             <input type="text" name="txtEmail"  class="required form-control" id="txtEmail" value="<?php if($strErrorMessage !="") echo $strEmail; ?>"/>
           </div>
           <div class="form-group">
-            <label>Phone *
+            <label>Company Phone Number
             </label>
             <input type="number" name="txtPhone"  class="required form-control" id="txtPhone" value="<?php if($strErrorMessage !="") echo $intPhoneNo; ?>"/>
-          </div>
-          <div class="form-group">
-            <label>Company Name
-            </label>
-            <input type="text" name="txtCompany" id="txtCompany" class="form-control" value="<?php if($strErrorMessage !="") echo $strCompany; ?>"/>
-          </div>                        
+          </div>                                  
         </div>
         <div class="col-sm-5">
           <div class="form-group">
-            <label>Website
+            <label>Company address
             </label>
-            <input type="text" name="txtWebsite" id="txtWebsite" class="form-control" value="<?php if($strErrorMessage !="") echo $strWebsite; ?>"/>
+            <input type="text" name="txtCompany" id="txtCompany" class="form-control" value="<?php if($strErrorMessage !="") echo $strCompany; ?>"/>
           </div>
           <div class="form-group">
-            <label>Message *
+            <label>Message
             </label>
             
             <textarea name="txtMessage" id="txtMessage" class="form-control" rows="8"> 
@@ -215,8 +184,7 @@ unset($_POST);
       <div class="row">
         <div class="col-sm-5 text-center">
           <div class="gmap">
-            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=JoomShaper,+Dhaka,+Dhaka+Division,+Bangladesh&amp;aq=0&amp;oq=joomshaper&amp;sll=37.0625,-95.677068&amp;sspn=42.766543,80.332031&amp;ie=UTF8&amp;hq=JoomShaper,&amp;hnear=Dhaka,+Dhaka+Division,+Bangladesh&amp;ll=23.73854,90.385504&amp;spn=0.001515,0.002452&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=1073661719450182870&amp;output=embed">
-            </iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.4453567369783!2d73.82046221489468!3d18.644000687337183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b814ecf1eff9%3A0xfa2719273af1506d!2sFour+Tech+Engineers+Private+Limited!5e0!3m2!1sen!2sin!4v1460382047718" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
         </div>
         <div class="col-sm-7 map-content">
